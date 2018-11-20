90f6ba0f5e022d80cd048b9ed019844b95fdf36e
Daniela Duarte <falecom@danieladuarte.com.br>
2018-11-20 15:31:56 -0200

    chore: updates dependencies

---
+++ build/precache-manifest.9db06cc332b2170dbc8b345e198ddf0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1329049d171a33b78e76dcb0debff61b",
+    "url": "/my-reads/static/media/logo.1329049d.png"
+  },
+  {
+    "revision": "f8765f1c36e7804293bb9070aa6e451d",
+    "url": "/my-reads/static/media/arrow-back.f8765f1c.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "f5bada306bbecfa2642a",
+    "url": "/my-reads/static/js/main.f5bada30.chunk.js"
+  },
+  {
+    "revision": "8f3946415c25a9c584a1",
+    "url": "/my-reads/static/js/1.8f394641.chunk.js"
+  },
+  {
+    "revision": "f5bada306bbecfa2642a",
+    "url": "/my-reads/static/css/main.753b7f04.chunk.css"
+  },
+  {
+    "revision": "9925ec4b1c160f2fe4ccb327b0e91756",
+    "url": "/my-reads/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57be9bbc555f10584c9d745c6b2974f5.js"
+  "/my-reads/precache-manifest.9db06cc332b2170dbc8b345e198ddf0d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-reads/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
