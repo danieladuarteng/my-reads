95407bb1ff987a1d032617735695e33c3b746c05
Daniela Duarte <falecom@danieladuarte.com.br>
2018-11-20 14:43:30 -0200

    feat: add debounce for requests reduction

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -16,7 +16,7 @@ class SearchBooks extends Component {
         result: [],
     }
 
-    search(query) {
+    search = debounce((query) => {
         this.setState({ query });
         if (query.trim().length) {
             BooksAPI.search(query.trim()).then((response) => {
@@ -41,10 +41,10 @@ class SearchBooks extends Component {
                 result: [],
             })
         }
-    }
+    }, 100);
 
     clearQuery = () => {
-        this.setState({ query: ''})
+        this.setState({ query: '' })
     }
 
     render() {
@@ -61,86 +61,86 @@ class SearchBooks extends Component {
 
         return (
             <div>
-            <div className="list-books" >
-                <div className="list-books-title-search">
-                    <h1>Search</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <div className='search-books-bar'>
-                            <Link className='close-search' to="/">Close</Link>
-                            <input
-                                className='search-books-bar'
-                                type='text'
-                                placeholder='Search new books'
-                                value={query}
-                                onChange={(event) => this.search(event.target.value)}
-                            />
-                        </div>
+                <div className="list-books" >
+                    <div className="list-books-title-search">
+                        <h1>Search</h1>
+                    </div>
+                    <div className="list-books-content">
+                        <div>
+                            <div className='search-books-bar'>
+                                <Link className='close-search' to="/">Close</Link>
+                                <input
+                                    className='search-books-bar'
+                                    type='text'
+                                    placeholder='Search new books'
+                                    value={query}
+                                    onChange={(event) => this.search(event.target.value)}
+                                />
+                            </div>
 
-                        {!showingBooks.length  && (
-                            <div className='message-error'>
-                                <p>We didn't find anything. Please try again with words:</p>
-                                <p>   'Android',
-                                    'Art', 'Artificial Intelligence', 'Astronomy',
-                                    'Austen', 'Baseball', 'Basketball', 'Bhagat',
-                                    'Biography', 'Brief', 'Business', 'Camus',
-                                    'Cervantes', 'Christie', 'Classics', 'Comics',
-                                    'Cook', 'Cricket', 'Cycling', 'Desai', 'Design',
-                                    'Development', 'Digital Marketing', 'Drama',
-                                    'Drawing', 'Dumas', 'Education', 'Everything',
-                                    'Fantasy', 'Film', 'Finance', 'First', 'Fitness',
-                                    'Football', 'Future', 'Games', 'Gandhi', 'Homer',
-                                    'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King',
-                                    'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make',
-                                    'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting',
-                                    'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming',
-                                    'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire',
-                                    'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale',
-                                    'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality',
-                                    'Web Development', 'iOS'
+                            {!showingBooks.length && (
+                                <div className='message-error'>
+                                    <p>We didn't find anything. Please try again with words:</p>
+                                    <p>   'Android',
+                                        'Art', 'Artificial Intelligence', 'Astronomy',
+                                        'Austen', 'Baseball', 'Basketball', 'Bhagat',
+                                        'Biography', 'Brief', 'Business', 'Camus',
+                                        'Cervantes', 'Christie', 'Classics', 'Comics',
+                                        'Cook', 'Cricket', 'Cycling', 'Desai', 'Design',
+                                        'Development', 'Digital Marketing', 'Drama',
+                                        'Drawing', 'Dumas', 'Education', 'Everything',
+                                        'Fantasy', 'Film', 'Finance', 'First', 'Fitness',
+                                        'Football', 'Future', 'Games', 'Gandhi', 'Homer',
+                                        'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King',
+                                        'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make',
+                                        'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting',
+                                        'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming',
+                                        'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire',
+                                        'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale',
+                                        'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality',
+                                        'Web Development', 'iOS'
                                 </p>
-                            </div>
-                        )}
+                                </div>
+                            )}
 
-                        {showingBooks.length !== books.length && (
-                            <div className='showing-contacts'>
-                                <h2>Now showing {showingBooks.length}</h2>
-                                <button onClick={this.clearQuery}>Show all</button>
-                            </div>
-                        )}
+                            {showingBooks.length !== books.length && (
+                                <div className='showing-contacts'>
+                                    <h2>Now showing {showingBooks.length}</h2>
+                                    <button onClick={this.clearQuery}>Show all</button>
+                                </div>
+                            )}
 
-                        <div className="bookshelf">
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    {showingBooks && showingBooks.map((book) => (
-                                        <li key={book.id}>
-                                            <Book
-                                                book={book}
-                                                imageLinks={book.imageLinks}
-                                                onChange={(e) => onChangeShelf(e, book)}
-                                                shelf={book.shelf}
-                                                title={book.title}
-                                            />
-                                        </li>
-                                    ))}
-                                </ol>
+                            <div className="bookshelf">
+                                <div className="bookshelf-books">
+                                    <ol className="books-grid">
+                                        {showingBooks && showingBooks.map((book) => (
+                                            <li key={book.id}>
+                                                <Book
+                                                    book={book}
+                                                    imageLinks={book.imageLinks}
+                                                    onChange={(e) => onChangeShelf(e, book)}
+                                                    shelf={book.shelf}
+                                                    title={book.title}
+                                                />
+                                            </li>
+                                        ))}
+                                    </ol>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
-                
-            </div>
-            <footer>Project Udacity My Reads - Developed by
+                <footer>Project Udacity My Reads - Developed by
             <a
-                href="https://github.com/danieladuarteng"
-                alt="GitHub of Daniela Duarte"
-                target="_blank"
-                rel="noopener noreferrer"
-            >Daniela Duarte - GitHub
+                        href="https://github.com/danieladuarteng"
+                        alt="GitHub of Daniela Duarte"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >Daniela Duarte - GitHub
             </a>
-        </footer>
-        </div>
+                </footer>
+            </div>
         )
     }
 
