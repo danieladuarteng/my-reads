12ab0a4e538303dd43c33ca0de6c97a079013649
Daniela Duarte <falecom@danieladuarte.com.br>
2018-11-25 14:18:04 -0200

    Updates

---
+++ precache-manifest.f6f4254d0995de9de98bc41758e9dc99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1329049d171a33b78e76dcb0debff61b",
+    "url": "/my-reads/static/media/logo.1329049d.png"
+  },
+  {
+    "revision": "f8765f1c36e7804293bb9070aa6e451d",
+    "url": "/my-reads/static/media/arrow-back.f8765f1c.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "f2be9c24eb8dafae130e",
+    "url": "/my-reads/static/js/main.f2be9c24.chunk.js"
+  },
+  {
+    "revision": "e26ebd1103420aa1bc0d",
+    "url": "/my-reads/static/js/1.e26ebd11.chunk.js"
+  },
+  {
+    "revision": "f2be9c24eb8dafae130e",
+    "url": "/my-reads/static/css/main.753b7f04.chunk.css"
+  },
+  {
+    "revision": "aec627e44c603bf0f9316171279e30fe",
+    "url": "/my-reads/index.html"
+  }
+];
