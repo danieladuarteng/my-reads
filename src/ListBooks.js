a1022e75378bb9aa994a2d6067e8ed9873df0e65
dasantos <daniela.santos@kestraa.com>
2018-11-28 16:57:57 -0200

    refactor: transform class in Functional Stateless Component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import logo from './images/logo.png'
@@ -20,51 +20,49 @@ const shelves = [
     },
 ];
 
-class ListBooks extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,//aqui falo que minha prop books precisa ser um array
-        onChangeShelf: PropTypes.func.isRequired,//e aqui que a onChangeShelf seja uma função
-    }
-
-    render() {
-        const { books, onChangeShelf } = this.props
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <img src={logo} alt="logo" />
-                    <Link
-                        to="/search"
-                        className="link-new-books"
-                    >
-                        <div className="new-books">
-                            <img src={add} alt="add" />
-                            <h1>Search new books</h1>
-                        </div>
-                    </Link>
-                </div>
-
-                {shelves.map(item => (
-                    <BookShelf
-                        key={item.title}
-                        books={books}
-                        title={item.title}
-                        shelf={item.shelf}
-                        onChangeShelf={onChangeShelf}
-                    />
-                ))}
-
-                <footer>Project Udacity My Reads - Developed by
-                    <a
-                        href="https://github.com/danieladuarteng"
-                        alt="GitHub of Daniela Duarte"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >Daniela Duarte - GitHub
-                    </a>
-                </footer>
+const ListBooks = ( { books, onChangeShelf } ) => {
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <img src={logo} alt="logo" />
+                <Link
+                    to="/search"
+                    className="link-new-books"
+                >
+                    <div className="new-books">
+                        <img src={add} alt="add" />
+                        <h1>Search new books</h1>
+                    </div>
+                </Link>
             </div>
-        )
-    }
-}
+
+            {shelves.map(item => (
+                <BookShelf
+                    key={item.title}
+                    books={books}
+                    title={item.title}
+                    shelf={item.shelf}
+                    onChangeShelf={onChangeShelf}
+                />
+            ))}
+
+            <footer>Project Udacity My Reads - Developed by
+                    <a
+                    href="https://github.com/danieladuarteng"
+                    alt="GitHub of Daniela Duarte"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >Daniela Duarte - GitHub
+                    </a>
+            </footer>
+        </div>
+    )
+};
+
+ListBooks.propTypes = {
+    books: PropTypes.array.isRequired,//aqui falo que minha prop books precisa ser um array
+    onChangeShelf: PropTypes.func.isRequired,//e aqui que a onChangeShelf seja uma função
+};
+
 
 export default ListBooks
